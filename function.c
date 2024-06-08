1ef964d772ff324baa43ca728d1dd679ad648b9d
DrsBlack <souzacleber.cs.cs@gmail.com>
2024-06-08 15:40:25 -0300

    Correção final

--- function.c
+++ function.c
@@ -1,41 +1,14 @@
 #include "function.h"
 
-// Definindo cores para estilização
-#define RESET "\x1b[0m"
-#define RED "\x1b[31m"
-#define GREEN "\x1b[32m"
-#define YELLOW "\x1b[33m"
-#define BLUE "\x1b[34m"
-#define MAGENTA "\x1b[35m"
-#define CYAN "\x1b[36m"
-#define WHITE "\x1b[37m"
-#define BRED "\x1b[91m"
-#define BGREEN "\x1b[92m"
-#define BYELLOW "\x1b[93m"
-#define BBLUE "\x1b[94m"
-#define BMAGENTA "\x1b[95m"
-#define BCYAN "\x1b[96m"
-#define BWHITE "\x1b[97m"
-#define BG_RED "\x1b[41m"
-#define BG_GREEN "\x1b[42m"
-#define BG_YELLOW "\x1b[43m"
-#define BG_BLUE "\x1b[44m"
-#define BG_MAGENTA "\x1b[45m"
-#define BG_CYAN "\x1b[46m"
-#define BG_WHITE "\x1b[47m"
-#define RESET "\x1b[0m"
-#define BOLD "\x1b[1m"
-#define ITALIC "\x1b[3m"
-#define UNDERLINE "\x1b[4m"
-
+// Constantes
 
 // Variáveis
 double valorConversao;
 
 // Funções
-#include <stdio.h>
-#include <time.h>
 
+
+// Função para limpar o buffer do teclado
 void limparBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF);
@@ -93,7 +66,7 @@ bool menuConversao(struct unidade inicio) {
     switch (opcao) {
         case OP_TEMPERATURA:
             valor = round(valor);
-            
+
             if (!temperatura(nome, valor)) {
                 return false;
             }
@@ -281,6 +254,8 @@ double Quilometros_para_Metros(double km) {
     return km * 1000.0;
 }
 
+// Código da função tempo
+
 double Dias_para_Horas(double dias) {
     return dias * 24.0;
 }
@@ -305,7 +280,7 @@ double Minutos_para_Horas(double minutos) {
     return minutos / 60.0;
 }
 
-// Código da função tempo
+
 bool tempo(int opcao, double valor) {
     double valorOriginal = valor;
 
---
+++ menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_MENU_H
+#define MENU_MENU_H
+
+#include "function.h"
+
+int menu();
+bool validarEntradaInt(const char *entrada);
+bool validarEntradaDouble(const char *entrada);
+int capturarInteiro();
+double capturarDouble();
+
+#endif // MENU_MENU_H
